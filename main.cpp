320a9a27041a210272f42bf23ad69f0985fa0ca2
g6nfm <fahey27j@gmail.com>
2024-11-23 23:13:23 -0500

    executeable created
    enjoy!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-#include "mainwindow.h"
-
+#include <windows.h>
 #include <QApplication>
+#include "mainwindow.h"
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
     return a.exec();
 }
